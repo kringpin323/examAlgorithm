70b876c163f3dbbd3b57ab2cab493aec432c9d2f
Kingpin <kringpin_lin@163.com>
2014-09-14 12:54:03 +0800

    平安科技挑战失败 通过这次的情况，发觉自己只是在应该走工程实现领域，而不是算法研究领域 人生苦短，还是不要折腾自己

--- src/protocol/SafeTest.java
+++ src/protocol/SafeTest.java
@@ -2,6 +2,8 @@ package protocol;
 
 import java.util.Arrays;
 
+// 通过这次的情况，发觉自己只是在应该走工程实现领域，而不是算法研究领域
+// 人生苦短，还是不要折腾自己
 public class SafeTest {
 	// TODO 数据结构
 	// 不会超过 30 行
@@ -15,11 +17,12 @@ public class SafeTest {
 		line line1 = new line();
 		line1.income = 0.00;
 		line1.spending = 51.90;
-		line1.balance = 1945.45;
+//		line1.balance = 1945.45;
 		
 		line line2 = new line();
 		line2.income = 0.00;
 		line2.spending = 1000.00;
+		line2.balance = 945.45;
 		
 		line[] linesA = new line[]{line1,line2};
 		count = linesA.length;
@@ -34,32 +37,35 @@ public class SafeTest {
 	
 	//TODO 写一个 method 逐行处理 输入数据
 	public void pretreatment(String oneLine){
-
 	}
 
 	//TODO 核心算法 第N-1行余额(+第N行收入或-第N行支出)=第N行余额  
 	public void queryBalance(line[] linesA){
-		// 三者都不是模糊，但 下一个的 balance是模糊的情况
-		//		if((!isNotNull(Lines[n].balance)) 
-		//				&& isNotNull(Lines[n-1].balance)
-		//				&&isNotNull(Lines[n].income)
-		//				&&isNotNull(Lines[n].spending)){
-		//			Lines[n].balance = Lines[n-1].balance + Lines[n].income - Lines[n].spending;
-		//		}
-
 		// 整张表都填满
 		for(int i=0;i<count;i++){
 			if(isNull(linesA[i].balance))
 				cacular(linesA,i,"balance");
 			if(isNull(linesA[i].spending) || isNull(linesA[i].income))
 					cacular(linesA,i,"SpIn");
-				
 		}
 	}
 
 	//TODO 这个递归计算出 A的值
 	public void cacular(line[] A,int nn, String type){
-		if(nn >= 0 && nn < count ){
+		// 值是0 特殊处理
+		// 为0的时候只有 balance 需要处理
+		if(nn == 0 ){
+			if(!isNull(A[nn+1].balance)
+					&& !isNull(A[nn+1].income)
+					&& !isNull(A[nn+1].spending))
+				A[nn].balance = A[nn+1].balance - A[nn+1].income
+				+ A[nn].spending;
+			else{
+				if(isNull(A[nn+1].balance))
+					cacular(A,nn+1,"balance");
+			}
+		}
+		if(nn >= 1 && nn <= count-1 ){
 			if(type == "balance"){
 				// 情况1
 				if(!isNull(A[nn-1].balance)
@@ -108,7 +114,6 @@ public class SafeTest {
 	}
 
 	//TODO 递归 计算 当缺失
-
 	public boolean isNull(double temp){
 		if(temp == Double.MAX_VALUE)
 			return true;
